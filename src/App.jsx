b652646cb6e0a7c9f828b5782f053b64f79f5977
dreiyn <dreiyn.temp@gmail.com>
2022-10-29 22:53:22 +0800

    [nike-store-shop]- add feature and highlight

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { Hero, Sales } from "./components";
-import { heroapi, popularsales, toprateslaes } from "./data/data";
+import { FlexContent, Hero, Sales } from "./components";
+import {
+  heroapi,
+  highlight,
+  popularsales,
+  sneaker,
+  toprateslaes,
+} from "./data/data";
 function App() {
   return (
     <>
       <main className="flex flex-col gap-16">
         <Hero heroapi={heroapi} />
         <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
         <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
       </main>
     </>
   );
